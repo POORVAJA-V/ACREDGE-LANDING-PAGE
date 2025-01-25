a9057d8ee3980d14b742be6cdf3dca57233c2718
POORVAJA-V <poorvajavijayan16@gmail.com>
2025-01-25 18:45:00 +0530

    initial commit

--- index.js
+++ index.js
@@ -8,3 +8,13 @@ $(document).ready(function () {
     $(this).parent().toggleClass("checked");
   });
 });
+$(".custom-carousel").owlCarousel({
+  autoWidth: true,
+  loop: true
+});
+$(document).ready(function () {
+  $(".custom-carousel .item").click(function () {
+    $(".custom-carousel .item").not($(this)).removeClass("active");
+    $(this).toggleClass("active");
+  });
+});
